b4d886eeea1b365e2429b8a1660651b6425d6ef3
xpialer2020 <xpialer@outlook.com>
2018-06-04 22:17:30 +0700

    Update v1.2
    Update admin page
      - admin1
      - admin2
      - admin3
    Edite style css

--- js/session.js
+++ js/session.js
@@ -2,17 +2,15 @@
 let userId = localStorage.getItem("session");
 if (userId === "undefined" || userId === null) {
     window.location.href = "../login.html";
-}else{
-    // session
-    let userId = localStorage.getItem("session");
-    // alert(userId);
-    firebase.database().ref('users/data/' + userId).once('value').then(function(snapshot) {
+} else {
+    firebase.database().ref('users/data/' + userId).once('value').then(function (snapshot) {
         $("#result").text(snapshot.val().name);
+        var account = snapshot.val().name;
     });
 }
 
 // logout
 function logout() {
-  localStorage.removeItem("session");
-  window.location.href = "../login.html";
+    localStorage.removeItem("session");
+    window.location.href = "../login.html";
 }
