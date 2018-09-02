67cbfa166c55e4c1efc6ce104bdc9ded294230e0
xpialer2020 <xpialer@outlook.com>
2018-09-03 02:00:44 +0700

    Well Done
    Very good

--- js/config.js
+++ js/config.js
@@ -6,7 +6,21 @@ var config = {
     projectId: "fitm-system-3",
     storageBucket: "fitm-system-3.appspot.com",
     messagingSenderId: "1083923833969"
-  };
+};
 firebase.initializeApp(config);
 var dbRef = firebase.database().ref();
 var database = firebase.database();
+
+// qrcode
+document.addEventListener('DOMContentLoaded', function () {
+    if($('#qrcode').length > 0) {
+        var qrcode = new QRCode("qrcode", {
+            text: "https://fitm-system-3.firebaseapp.com/report.html",
+            width: 128,
+            height: 128,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+    }
+}, false);
