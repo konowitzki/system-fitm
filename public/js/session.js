ff51ffdb6b421268611ddf5d4deb3c01824be0e3
kittapas <ko_msm1@hotmail.com>
2018-09-06 02:08:04 +0700

    add public folder
    for firebase deploy

---
+++ public/js/session.js
@@ -0,0 +1,16 @@
+// Session
+let userId = localStorage.getItem("session");
+if (userId === "undefined" || userId === null) {
+    window.location.href = "../login.html";
+} else {
+    firebase.database().ref('users/data/' + userId).once('value').then(function (snapshot) {
+        $("#result").text(snapshot.val().name);
+        var account = snapshot.val().name;
+    });
+}
+
+// logout
+function logout() {
+    localStorage.removeItem("session");
+    window.location.href = "../login.html";
+}
