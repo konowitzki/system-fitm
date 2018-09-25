5b1b567789b55bb318f27625d9dee587e331076c
kittapas <ko_msm1@hotmail.com>
2018-09-25 08:11:01 +0700

    deploy
    new code to deploy

--- public/js/session.js
+++ public/js/session.js
@@ -1,16 +1,20 @@
 // Session
 let userId = localStorage.getItem("session");
 if (userId === "undefined" || userId === null) {
-    window.location.href = "../login.html";
+  window.location.href = "../index.html";
 } else {
-    firebase.database().ref('users/data/' + userId).once('value').then(function (snapshot) {
-        $("#result").text(snapshot.val().name);
-        var account = snapshot.val().name;
+  firebase
+    .database()
+    .ref("users/data/" + userId)
+    .once("value")
+    .then(function(snapshot) {
+      $("#result").text(snapshot.val().name);
+      var account = snapshot.val().name;
     });
 }
 
 // logout
 function logout() {
-    localStorage.removeItem("session");
-    window.location.href = "../login.html";
+  localStorage.removeItem("session");
+  window.location.href = "../index.html";
 }
